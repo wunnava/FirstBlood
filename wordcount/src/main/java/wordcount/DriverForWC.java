0b84f25d59c0e04867823fd425e0664c7ab4489e
wunnava <venkata.wunnava@cloudwick.com>
2014-02-06 13:26:20 -0800

    sf

--- wordcount/src/main/java/wordcount/DriverForWC.java
+++ wordcount/src/main/java/wordcount/DriverForWC.java
@@ -1,9 +1,13 @@
+package wordcount;
 
-
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -11,6 +15,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+
 
 public class DriverForWC extends Configured implements Tool {
 
@@ -25,15 +30,18 @@ public class DriverForWC extends Configured implements Tool {
 		
 		@SuppressWarnings("deprecation")
 		Job job = new Job();
+		JobConf jobconf = new JobConf(new Configuration(), DriverForWC.class);
+		jobconf.setJarByClass(DriverForWC.class);
+		jobconf.setQueueName("testing");
 		job.setJarByClass(DriverForWC.class);
 		job.setJobName(getClass().getName());
-		
+				
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
 		job.setMapperClass(MapperForWC.class);
 		job.setReducerClass(ReducerForWC.class);
-		job.setCombinerClass(ReducerForWC.class);
+		job.setCombinerClass(CombinerForWC.class);
 		
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
--- wordcount/src/main/java/wordcount/MapperForWC.java
+++ wordcount/src/main/java/wordcount/MapperForWC.java
@@ -1,3 +1,4 @@
+package wordcount;
 
 import java.io.IOException;
 
